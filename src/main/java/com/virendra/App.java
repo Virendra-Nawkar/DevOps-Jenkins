2dfc53067f82be8281b2a1580d525826427edc33
Virendra Nawkar <virendranawkar@Virendras-MacBook-Air.local>
2026-02-21 11:20:30 +0530

    New Deploy

--- src/main/java/com/virendra/App.java
+++ src/main/java/com/virendra/App.java
@@ -6,5 +6,6 @@ package com.virendra;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World Jenkins Virendra!");
+        System.out.println("Auto Build");
     }
 }
