bb3e545a12a6bf682c29770a95f0d18f1a07591d
Virendra Nawkar <virendranawkar@Virendras-MacBook-Air.local>
2026-02-21 10:40:02 +0530

    Initial commit

---
+++ src/main/java/com/virendra/App.java
@@ -0,0 +1,10 @@
+package com.virendra;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World Jenkins Virendra!");
+    }
+}
